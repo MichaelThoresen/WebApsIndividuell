34e71f8e099454075199e27e3bb9425e643a6505
MichaelThoresen <mimothor@hotmail.com>
2019-11-11 14:52:00 +0100

    Added Get methods for question and dto plus mapping

---
+++ VySPA/Db/Repository/FAQrepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VySPA.Models.DTO;
+using VySPA.Models.Entities;
+
+namespace VySPA.Db.Repository
+{
+    public interface FAQrepo
+    {
+        List<Question> FindAllQuestions();
+
+        List<QuestionDTO> GetQuestionDTOs();
+
+        QuestionDTO MapQuestionDTO();
+    }
+}
---
+++ VySPA/Db/Repository/FAQrepoImpl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VySPA.Models.DTO;
+using VySPA.Models.Entities;
+
+namespace VySPA.Db.Repository
+{
+    public class FAQrepoImpl : FAQrepo
+    {
+        private readonly FaQDbContext _context;
+        private List<Question> FindAllQuestions()
+        {
+            return _context.question.ToList();
+        }
+
+        private QuestionDTO MapQuestionDTO(Question q)
+        {
+            return new QuestionDTO
+            {
+                Id = q.Id,
+                QuestionText = q.QuestionText,
+                AnswerText = q.AnswerText
+            };
+        }
+
+        private List<QuestionDTO> GetQuestionDTOs()
+        {
+            List<Question> Questions = FindAllQuestions();
+            var dtos = new List<QuestionDTO>();
+            foreach(var q in Questions)
+            {
+                MapQuestionDTO(q);
+            }
+
+            return dtos;
+        }
+    }
+}
