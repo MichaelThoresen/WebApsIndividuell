6e2f6f8fbaca8c00519ec37f5bea6ec1578f8865
MichaelThoresen <32193552+MichaelThoresen@users.noreply.github.com>
2019-11-16 18:57:55 +0100

    Finalized finalizing

--- VyFaQ2/Model/DTO/UserQuestionDTO.cs
+++ VyFaQ2/Model/DTO/UserQuestionDTO.cs
@@ -6,10 +6,14 @@ namespace VyFaQ2.Model.DTO
     public class UserQuestionDTO
     {
         [Key]
+        [EmailAddress]
+        [Required]
         public String Email { get; set; }
 
+        [Required]
         public String QuestionText { get; set; }
 
+        [Required]
         public String Name { get; set; }
     }
 }
